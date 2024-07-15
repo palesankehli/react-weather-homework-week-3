f50967a3fe558c228e49654d1eec230b904a8b9a
Palesa Nkehli <93646366+palesankehli@users.noreply.github.com>
2024-07-15 18:37:40 +0200

    included Github link

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Weather from "./Weather";
+import Links from "./links";
 
 import App from "./App";
 
@@ -11,5 +12,6 @@ root.render(
   <StrictMode>
     <App />
     <Weather />
+    <Links />
   </StrictMode>
 );
---
+++ src/links.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Links() {
+  return (
+    <div className="Links">
+      <p>
+        This project is on{" "}
+        <a href="https://github.com/palesankehli/react-weather-homework-week-3">
+          Github
+        </a>
+      </p>
+    </div>
+  );
+}
